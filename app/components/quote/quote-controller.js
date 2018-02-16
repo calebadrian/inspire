7e647f234abd919eae8e15761ef4c6a4f1c2d76a
Caleb Adrian <calebadrian1847@gmail.com>
2018-02-16 10:30:04 -0700

    quote drawing to page

--- app/components/quote/quote-controller.js
+++ app/components/quote/quote-controller.js
@@ -1,8 +1,19 @@
 function QuoteController(){
 
 	var qs = new QuoteService()
+	var quoteElem = document.getElementById('quote')
 
-	qs.getQuote(function(quote){
-		console.log('What is the quote', quote)
-	})
+	// qs.getQuote(function(quote){
+	// 	console.log('What is the quote', quote)
+	// })
+
+	function draw(quote){
+		var template = ''
+		template += `
+		${quote.quote}
+		${quote.author}`
+		quoteElem.innerHTML = template
+	}
+
+	qs.getQuote(draw)
 }
