af9a0c5c57e4eb6534f51c3609b2a4041c400b45
Caleb Adrian <calebadrian1847@gmail.com>
2018-02-19 09:55:19 -0700

    Formatting changes and user persistance

--- app/components/time/time-controller.js
+++ app/components/time/time-controller.js
@@ -1,5 +1,8 @@
 function TimeController(){
 
+    var timeService = new TimeService()
+    var userName = document.getElementById('user-name')
+
     function twelveClock() {
         var now = new Date()
         var hours = now.getHours() > 12 ? now.getHours() - 12 : now.getHours()
@@ -9,11 +12,11 @@ function TimeController(){
         var time = hours + ':' + minutes + ':' + seconds + ' ' + amPm
 		var template = `<h1 class="bg-style">${time}</h1>`
 		if (now.getHours() < 12){
-			template += `<h3 class="bg-style">Good Morning, Caleb</h3>`
-		} else if (now.getHours() > 12 && now.getHours() < 18){
-			template += `<h3 class="bg-style">Good Afternoon, Caleb</h3>`
+			template += `<h3 class="bg-style">Good Morning</h3>`
+		} else if (now.getHours()> 12 && now.getHours() < 18){
+			template += `<h3 class="bg-style">Good Afternoon</h3>`
 		} else if (now.getHours() >= 18){
-			template += `<h3 class="bg-style">Good Evening, Caleb</h3>`
+			template += `<h3 class="bg-style">Good Evening</h3>`
 		}
 		document.getElementById('twelve-time').innerHTML = template
         
@@ -27,19 +30,25 @@ function TimeController(){
         var seconds = now.getSeconds() < 10 ? '0' + now.getSeconds() : now.getSeconds()
         var time = hours + ':' + minutes + ':' + seconds
 		var template = `<h1 class="bg-style">${time}</h1>`
-		if (now.getHours() < 12){
-			template += `<h3 class="bg-style">Good Morning, Caleb</h3>`
-		} else if (now.getHours() > 12 && now.getHours() < 18){
-			template += `<h3 class="bg-style">Good Afternoon, Caleb</h3>`
-		} else if (now.getHours() >= 18){
-			template += `<h3 class="bg-style">Good Evening, Caleb</h3>`
+		if (hours < 12){
+			template += `<h3 class="bg-style">Good Morning</h3>`
+		} else if (hours >= 12 && hours < 18){
+			template += `<h3 class="bg-style">Good Afternoon</h3>`
+		} else if (hours >= 18){
+			template += `<h3 class="bg-style">Good Evening</h3>`
 		}
 		document.getElementById('twentyfour-time').innerHTML = template
         
 		setTimeout(twentyfourClock, 1000);
     }
-    twelveClock()
-    twentyfourClock()
+
+    function drawUserName(res){
+        if (res.length < 1){
+            userName.innerText = 'Add a user to see your name here'
+            return
+        }
+        userName.innerText = `${res[0].name}`
+    }
 
     this.toggleClock = function toggleClock(){
         var twelve = document.getElementById('twelve-time')
@@ -52,6 +61,31 @@ function TimeController(){
             twentyfour.classList.remove('hidden')
         }
     }
+
+    this.showAddUserForm = function showAddUserForm(){
+        var userElem = document.getElementById('user-form')
+        userElem.classList.remove('hidden')
+    }
+
+    this.addUser = function addUser(event){
+        event.preventDefault()
+        var form = event.target
+        timeService.addUser(form, drawUserName)
+        document.getElementById('user-form').classList.add('hidden')
+        form.reset()
+    }
+
+    this.editUser = function editUser(event){
+        event.preventDefault()
+        var form = event.target
+        timeService.changeName(form.name.value, drawUserName)
+        document.getElementById('user-form').classList.add('hidden')
+        form.reset()
+    }
+
+    twelveClock()
+    twentyfourClock()
+    timeService.getUsers(drawUserName)
 
 
 }
